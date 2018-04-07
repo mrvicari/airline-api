f96e5318eb21be7c50623fe75becc42b2c472983
Miguel Restrepo Vicari <sc15mrv@leeds.ac.uk>
2018-04-07 16:11:15 +0100

    Handle non existing airport

--- airline/views.py
+++ airline/views.py
@@ -21,7 +21,7 @@ def find_flight(request):
     try:
         dep_airport = Airport.objects.get(name__contains=body['dep_airport'])
         dest_airport = Airport.objects.get(name__contains=body['dest_airport'])
-    except ObjectDoesNotExist:
+    except Airport.DoesNotExist:
         return HttpResponse('No flights found', status=503)
 
     num_passengers = body['num_passengers']
